74bde034aa0c9fb95f0f903b3c3d2d547411082c
Konstantin Antonov <knstantna@gmail.com>
2024-03-07 17:43:21 +0300

    Back | add some sql tables

---
+++ src/data/sql/test_data/loads/tank.sql
@@ -0,0 +1,11 @@
+-- Переменный жидкий груз - цистерна
+
+INSERT INTO tank
+  (project_id, ship_id, tank_id, key, value)
+VALUES
+  (NULL, 1, 1, 'density', 1),
+  (NULL, 1, 1, 'volume', 1000),
+  (NULL, 1, 1, 'bound_x1', -10),
+  (NULL, 1, 1, 'bound_x2', 4),
+  (NULL, 1, 1, 'bound_y1', -2),
+  (NULL, 1, 1, 'bound_y2', 0);
---
+++ src/data/sql/test_data/loads/tank_center.sql
@@ -0,0 +1,9 @@
+-- Координаты центра объема жидкости в цистерне в системе координат судна
+-- в зависимости от объема жидкости в цистерне
+
+INSERT INTO tank_center
+  (project_id, ship_id, tank_id, key, value_x, value_y, value_z)
+VALUES
+  (NULL, 1, 1, 0, 0, 0, 0),
+  (NULL, 1, 1, 10, 1, 1, 1);
+
