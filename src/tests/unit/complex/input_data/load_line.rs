9a4e93e9bde28e2f297cb99aa2841c0382302c90
KnstAnt <knstantna@gmail.com>
2024-09-18 17:10:13 +0300

    Back | fix load_line

--- src/tests/unit/complex/input_data/load_line.rs
+++ src/tests/unit/complex/input_data/load_line.rs
@@ -3,9 +3,9 @@ use crate::data::structs::LoadLineDataArray;
 #[allow(dead_code)]
 pub(crate) fn load_line() -> LoadLineDataArray {
     LoadLineDataArray::from(vec![
-        ("Summer PS", 0., 6.7, 4.6),
-        ("Summer SB", 0., -6.7, 4.708),
-        ("Fresh water summer PS", 0.425, 6.7, 4.708),
-        ("Fresh water summer SB", 0.425, -6.7, 4.708),
+        ("LL draft S PS", 0., -6.7, 4.6),
+        ("LL draft S SB", 0., 6.7, 4.708),
+        ("LL draft F PS", 0.425, -6.7, 4.708),
+        ("LL draft F SB", 0.425, 6.7, 4.708),
     ])
 }
