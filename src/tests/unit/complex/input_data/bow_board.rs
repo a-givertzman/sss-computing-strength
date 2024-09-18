6956410f2749f966007fd50749ee880c40e247fa
KnstAnt <knstantna@gmail.com>
2024-09-18 11:08:23 +0300

    Back | fix test

---
+++ src/tests/unit/complex/input_data/bow_board.rs
@@ -0,0 +1,8 @@
+
+#[allow(dead_code)]
+pub(crate) fn bow_board() -> BowBoardDataArray {
+    BowBoardDataArray::from(vec![
+        ("Board on forward perpendicular SB", 59.193, 1.251, 9.696),
+        ("Board on forward perpendicular PS", 59.193, -1.251, 9.696),
+    ])
+}
